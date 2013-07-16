706bee0a5bf1760087788a3a57f49dd2a09e91ce
Roy van de Water <dev+roy@integrumtech.com>
2013-07-16 13:07:31 -0700

    Parse basic boolean flags

--- src/optparse/core.clj
+++ src/optparse/core.clj
@@ -1,6 +1,12 @@
-(ns optparse.core)
+(ns optparse.core
+  (:use [clojure.string :only (replace)])
+  (:refer-clojure :exclude [replace]))
+
+(defn- create-key [string]
+  (keyword (replace string "--" "")))
 
 (defn optparse
   "Parse options, returns array with hash of named arguments, 
    array of the rest of the arguments, and usage message"
-  [])
+  ([args & expectations]
+    (hash-map (create-key (first args)) true)))
--- test/optparse/core_test.clj
+++ test/optparse/core_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [optparse.core :refer :all]))
 
-(deftest method-exists
-  (testing "optparse method exists"
-    (is optparse)))
+(testing "handling no-argument flags"
+  (deftest should-handle-true-flag
+    (is (= {:help true} (optparse ["--help"] 
+                                  ["-h" "--help"]))))
+  (deftest should-handle-another-true-flag
+    (is (= {:verbose true} (optparse ["--verbose"]
+                                     ["-v" "--verbose"])))))
+
+; (testing "handling default values for flags not passed in"
+;   (deftest should-return-default-value-when-no-flag
+;     (is (= {:number 5} (optparse []
+;                                  ["-n" "--number=[value]" :default 5])))))
