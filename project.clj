3b4279fe8da9adc6601ff12de62d1582b839e856
Roy van de Water <dev+roy@integrumtech.com>
2013-07-16 09:40:55 -0700

    Add license information

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject optparse "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Option parser following standard UNIX conventions"
+  :url "http://github.com/royvandewater/optparse"
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
